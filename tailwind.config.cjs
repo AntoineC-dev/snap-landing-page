49c7e7025a7e76f1f695a92b668fe54d19918593
Antoine Cheminat <ac.dev@hotmail.com>
2022-07-30 07:49:10 -0400

    Add custom breakpoint: 1366px

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require("tailwindcss/defaultTheme");
+const { createTrue } = require("typescript");
 module.exports = {
   content: ["./src/app.html", "./src/**/*.{html,js,svelte,ts}"],
   theme: {
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1366px",
+      "3xl": "1536px",
+    },
     colors: {
       transparent: "transparent",
       current: "currentColor",
@@ -10,6 +19,9 @@ module.exports = {
       black: "hsl(0, 0%, 8%)",
       surface: "hsl(0, 0%, 98%)",
       gray: "hsl(0, 0%, 41%)",
+    },
+    container: {
+      center: true,
     },
     extend: {
       fontFamily: {
