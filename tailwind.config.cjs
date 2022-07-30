c687c7602acf12843886630386349f0f4861d991
Antoine Cheminat <ac.dev@hotmail.com>
2022-07-29 23:01:33 -0400

    Add colors & typography

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,19 +3,15 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./src/**/*.{html,js,svelte,ts}"],
   theme: {
-    // colors: {
-    //   transparent: "transparent",
-    //   current: "currentColor",
-    //   black: "hsl(0, 0%, 8%)",
-    //   white: "hsl(0, 0%, 98%)",
-    //   gray: "hsl(0, 0%, 41%)",
-    // },
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      white: "#ffffff",
+      black: "hsl(0, 0%, 8%)",
+      surface: "hsl(0, 0%, 98%)",
+      gray: "hsl(0, 0%, 41%)",
+    },
     extend: {
-      colors: {
-        black: "hsl(0, 0%, 8%)",
-        white: "hsl(0, 0%, 98%)",
-        gray: "hsl(0, 0%, 41%)",
-      },
       fontFamily: {
         sans: ["Epilogue", ...defaultTheme.fontFamily.sans],
       },
