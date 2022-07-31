5edc183ca6ce510bc194005d59b43b93d1e59f2e
Antoine Cheminat <ac.dev@hotmail.com>
2022-07-30 22:06:25 -0400

    Disable mouseEnter callback on mobile devices

--- src/components/DropdownMenu.svelte
+++ src/components/DropdownMenu.svelte
@@ -4,7 +4,7 @@
   import { quadInOut } from "svelte/easing";
   import { type DropdownMenu, navbarStore, setOpenDropdown } from "../stores/navbar.store";
   import { searchActiveNavlink } from "../helpers/searchActiveNavlink";
-  import { clickOutside, mouseOut } from "../helpers/mouseEvents";
+  import { mouseOut } from "../helpers/mouseEvents";
   import NavlinkItem from "./NavlinkItem.svelte";
   import src from "../assets/icon-arrow-down.svg";
 
@@ -13,9 +13,13 @@
   onMount(() => {
     semanticId = `${dropdown.label.toLowerCase()}-dropdown`;
   });
+  let innerWidth = 0;
+  $: mobileScreen = innerWidth < 768;
   $: open = $navbarStore.openDropdown === dropdown.id;
   $: active = searchActiveNavlink({ activeNavitem: $navbarStore.activeNavitem, items: dropdown.items });
 </script>
+
+<svelte:window bind:innerWidth />
 
 <div use:mouseOut={{ callback: () => setOpenDropdown(null), listen: open }} class="relative">
   <button
@@ -23,7 +27,8 @@
     aria-expanded={open}
     aria-controls={semanticId}
     type="button"
-    on:mouseenter={() => !open && setOpenDropdown(dropdown.id)}
+    on:mouseenter={() => !mobileScreen && !open && setOpenDropdown(dropdown.id)}
+    on:click={() => setOpenDropdown(open ? null : dropdown.id)}
     class="flex items-center p-2 gap-2 hover:text-black transition-colors rounded-2xl focus-ring {active
       ? 'text-black font-semibold'
       : ''}"
@@ -35,7 +40,6 @@
     <ul
       id={semanticId}
       transition:slide={{ duration: 300, easing: quadInOut }}
-      use:clickOutside={() => open && setOpenDropdown(null)}
       class="md:absolute min-w-max top-full {dropdown.anchor === 'left'
         ? 'left-0'
         : 'right-0'} flex flex-col md:bg-white md:shadow-lg px-8 py-4  md:p-4 rounded-xl"
--- src/helpers/mouseEvents.ts
+++ src/helpers/mouseEvents.ts
@@ -1,29 +1,28 @@
-export function clickOutside(element: Element, callbackFunction: () => void) {
-  function onClick(event: MouseEvent) {
-    console.log("clickOutside");
-    if (!element.contains(event.target as Node)) {
-      callbackFunction();
-    }
-  }
-
-  document.body.addEventListener("click", onClick);
-
-  return {
-    update(newCallbackFunction: () => void) {
-      callbackFunction = newCallbackFunction;
-    },
-    destroy() {
-      document.body.removeEventListener("click", onClick);
-    },
-  };
-}
-
-interface MouseOut {
+interface MouseEventProps {
   callback: () => void;
   listen?: boolean; // Listens by default
 }
 
-export function mouseOut(element: Element, props: MouseOut) {
+// export function clickOutside(element: Element, callbackFunction: () => void) {
+//   function onClick(event: MouseEvent) {
+//     if (!element.contains(event.target as Node)) {
+//       callbackFunction();
+//     }
+//   }
+
+//   document.body.addEventListener("click", onClick);
+
+//   return {
+//     update(newCallbackFunction: () => void) {
+//       callbackFunction = newCallbackFunction;
+//     },
+//     destroy() {
+//       document.body.removeEventListener("click", onClick);
+//     },
+//   };
+// }
+
+export function mouseOut(element: Element, props: MouseEventProps) {
   function onMouseOver(event: MouseEvent) {
     if (!element.contains(event.target as Node)) {
       props.callback();
@@ -35,7 +34,7 @@ export function mouseOut(element: Element, props: MouseOut) {
   }
 
   return {
-    update(newProps: MouseOut) {
+    update(newProps: MouseEventProps) {
       props = { ...newProps };
       if (props.listen === undefined || props.listen) {
         document.body.addEventListener("mouseover", onMouseOver);
